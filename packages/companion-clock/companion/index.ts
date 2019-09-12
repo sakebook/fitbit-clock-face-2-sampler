28cfdb3c65aa1159cce1e0468a044a9eec3534f5
sakebook <sakebook@gmail.com>
2019-09-12 19:24:26 +0900

    Use location

--- packages/companion-clock/companion/index.ts
+++ packages/companion-clock/companion/index.ts
@@ -1,15 +1,15 @@
 import * as messaging from "messaging";
 import { me } from "companion";
+import { geolocation } from "geolocation";
 
 const API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 const ENDPOINT = "https://api.openweathermap.org/data/2.5/weather";
 
-function createQuery(): string {
+function createQuery(latitude: number, longitude: number): string {
   return (
     ENDPOINT +
-    "?q=Tokyo,jp" +
+    `?lat=${latitude}&lon=${longitude}` +
     "&units=metric" +
-    "&lang=ja" +
     `&APPID=${API_KEY}`
   );
 }
@@ -23,28 +23,46 @@ function returnWeatherData(data: { temperature: number }): void {
   }
 }
 
-function fetchWeatherCompanion(): void {
+function isValidPermission(): boolean {
   if (!me.permissions.granted("access_internet")) {
-    console.log("We're allowed to access the internet!");
-    returnWeatherData(null);
-    return;
+    console.log("We're not allowed to access the internet!");
+    return false;
   }
-  fetch(createQuery())
-    .then(response => {
-      response.json().then(data => {
-        const weather: { temperature: number } = {
-          temperature: data["main"]["temp"] as number
-        };
-        returnWeatherData(weather);
-      });
-    })
-    .catch(err => {
+  if (!me.permissions.granted("access_location")) {
+    console.log("We're not allowed to access the location!");
+    return false;
+  }
+  return true;
+}
+
+function fetchWeatherCompanion(): void {
+  geolocation.getCurrentPosition(
+    (position: Position) => {
+      fetch(createQuery(position.coords.latitude, position.coords.longitude))
+        .then(response => {
+          response.json().then(data => {
+            const weather: { temperature: number } = {
+              temperature: data["main"]["temp"] as number
+            };
+            returnWeatherData(weather);
+          });
+        })
+        .catch(err => {
+          returnWeatherData(null);
+        });
+    },
+    err => {
       returnWeatherData(null);
-    });
+    }
+  );
 }
 
 messaging.peerSocket.onmessage = (evt: MessageEvent): void => {
   if (evt.data && evt.data.command == "weather") {
+    if (!isValidPermission()) {
+      returnWeatherData(null);
+      return;
+    }
     fetchWeatherCompanion();
   }
 };
